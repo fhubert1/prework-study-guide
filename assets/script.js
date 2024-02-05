2f5384389a58c7a6da90fa4432fd586c4e4cfa2e
fhubert1 <f.hubert.712@gmail.com>
2024-02-05 14:46:15 -0500

    added function logic and information to study guide

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,32 @@
+
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomtopic = topics[Math.floor(Math.random() * topics.length)];
 
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-}
-*/
+function listTopics() {
 
-for (let x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+    for (let x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+    
 }
+
+function selectTopics() {
+
+    if (randomtopic === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (randomtopic === 'CSS') {
+    console.log("Let's study CSS!");
+    } else if (randomtopic === 'Git') {
+    console.log("Let's study Git!");
+    } else if (randomtopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    } else {
+    console.log('Please try again!');
+    }
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopics();
+
